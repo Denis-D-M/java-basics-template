ba19384b15a451f30c3d32b86fb4778042226ee6
Denis Mishin <Denis_Mishin@epam.com>
2021-01-19 17:41:21 +0400

    Добавил lombok

--- src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
@@ -1,6 +1,11 @@
 package com.epam.izh.rd.online.service;
 
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class SimpleMathService implements MathService {
 
     @Override
@@ -18,76 +23,49 @@ public class SimpleMathService implements MathService {
         Arrays.sort(values);
         return values[values.length - 1];
     }
-    /**
-     * Метод возвращает сумму чисел массива.
-     * Например для списка {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть 30
-     */
+
     @Override
     public int sum(int[] values) {
         return Arrays.stream(values).sum();
     }
 
-    /**
-     * Метод фильтрует массив, оставляя только четные числа.
-     * Например для списка {-1, -3, 4, 8, 5, 22, 17} метод должен вернуть {4, 8, 22}
-     */
     @Override
     public int[] getEvenDigits(int[] values) {
-        return new int[]{};
+        return Arrays.stream(values).filter(value -> value % 2 == 0).toArray();
     }
 
-    /**
-     * Метод считает факториал из заданного числа.
-     * Например для числа 5 метод должен вернуть 120.
-     * Факториал 0 должен быть равен 1.
-     */
     @Override
     public long calcFactorial(int initialVal) {
-        return -1L;
+        return (initialVal == 0) ? 1 : IntStream.rangeClosed(1, initialVal).reduce((left, right) -> left * right).getAsInt();
     }
-
-    /**
-     * Метод возвращает число, которе находится на заданной позиции (счет начинается с нуля) в ряду фибоначчи.
-     *
-     * Ряд фибоначчи - ряд, следующие элементы которого состоят из суммы двух предыдущих.
-     * Ряд начинается 0 и 1.
-     * Пример 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ...
-     *
-     * Для числа 9 метод должен вернуть 34
-     * Для числа 0 метод должен вернуть 0
-     */
+    // не вижу смысла в long здесь
     @Override
     public long calcFibonacci(int number) {
-        return -1L;
+        return (long) Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
+                .skip(number)
+                .map(n -> n[0])
+                .limit(1)
+                .collect(Collectors.toList())
+                .get(0);
     }
 
-    /**
-     * Метод возвращает отсортированный по возрастанию массив.
-     * Например для массива {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть {-5, -3, -1, 4, 5, 8, 22}
-     */
     @Override
     public int[] sort(int[] values) {
-        return new int[]{};
+        Arrays.sort(values);
+        return values;
     }
 
-    /**
-     * Метод определяет, является ли заданное число простым.
-     * Простое число - число, которое делится только на 1 и на само себя.
-     *
-     * Например для числа 22 вернется false, а для числа 23 true.
-     */
     @Override
     public boolean isPrimary(int number) {
-        return false;
+        return BigInteger.valueOf(number).isProbablePrime(1);
     }
 
-    /**
-     * Метод возвращает массив, в котором элементы расположены в обратном порядке.
-     *
-     * Например для массива {-1, -3, 4, 8, 5, 22, -5} метод вернет {-5, 22, 5, 8, 4, -3, -1}
-     */
     @Override
     public int[] reverseArray(int[] values) {
-        return new int[]{};
+        int[] newArray = new int[values.length];
+        for (int i = values.length - 1, j = 0; i >= 0 ; i--, j++) {
+            newArray[j] = values[i];
+        }
+        return newArray;
     }
 }
